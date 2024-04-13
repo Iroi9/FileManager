5bb158146fc81a7c4160b9cedea5921cafef1b17
Ruben Minicozzi <52464838+Iroi9@users.noreply.github.com>
2024-04-14 01:08:59 +0200

    First commit

---
+++ DirectoryItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+
+public class DirectoryItem
+{
+    public string Name { get; set; }
+    public string FullPath { get; set; }
+    public ObservableCollection<DirectoryItem> SubDirectories { get; set; }
+
+    public DirectoryItem()
+    {
+        SubDirectories = new ObservableCollection<DirectoryItem>();
+    }
+
+    public void PopulateSubDirectories()
+    {
+        try
+        {
+            string[] subdirectoryEntries = Directory.GetDirectories(FullPath);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(subdirectory);
+                var subDirectoryItem = new DirectoryItem
+                {
+                    Name = directoryInfo.Name,
+                    FullPath = directoryInfo.FullName
+                };
+                subDirectoryItem.PopulateSubDirectories(); // Recursively populate subdirectories
+                SubDirectories.Add(subDirectoryItem);
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            // Handle unauthorized access to directories
+        }
+        catch (DirectoryNotFoundException)
+        {
+            // Handle directory not found exception
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace FileManager
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<DirectoryItem> Directories { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Directories = new ObservableCollection<DirectoryItem>();
+            treeView.ItemsSource = Directories;
+
+            // Load root directories
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                Directories.Add(new DirectoryItem { Name = drive.Name, FullPath = drive.RootDirectory.FullName });
+            }
+        }
+
+        private void treeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var selectedItem = (DirectoryItem)treeView.SelectedItem;
+            if (selectedItem != null)
+            {
+                PopulateListView(selectedItem.FullPath);
+            }
+        }
+
+        private void PopulateListView(string path)
+        {
+            listView.Items.Clear();
+            DirectoryInfo directory = new DirectoryInfo(path);
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                listView.Items.Add(new FileItem
+                {
+                    Name = file.Name,
+                    Size = file.Length,
+                    DateModified = file.LastWriteTime
+                });
+            }
+        }
+    }
+
+    public class FileItem
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
